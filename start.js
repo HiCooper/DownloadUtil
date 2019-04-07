04ad09f769cc0b42fed04b7771715de8408d4748
HiCooper <294237781@qq.com>
2019-04-07 10:47:20 +0800

    remove temp file

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
 let targetUrl = "https://jaist.dl.sourceforge.net/project/pentaho/Pentaho 8.2/client-tools/pdi-ce-8.2.0.0-342.zip";
 let download = require('./DownloadUtil')
 
-download(targetUrl, './')
+download(targetUrl, './temp/')
